2f469f49a016c661f9d788a82f955258e49d12a4
jrnold <jeffrey.arnold@gmail.com>
2016-05-04 11:24:37 -0700

    change travis deploy
    No longer use git subtree, instead use git push -f of that directory

--- _deploy.sh
+++ _deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 BUILD_DIR=${1:-_book}
-git subtree push --prefix "$BUILD_DIR" origin gh-pages
+cd $BUILD_DIR
+git init
+git add .
+git commit -m "Travis update"
+git push -f "https://${GH_TOKEN}@github.com/${GH_REF}/" master:gh-pages 2>&1 | sed -e "s/${GH_TOKEN}/XXXX/g"
