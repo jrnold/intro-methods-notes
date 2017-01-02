12518311677c8853cd8cfe7e25193da4ded39cf3
jrnold <jeffrey.arnold@gmail.com>
2017-01-01 19:51:04 -0800

    misc

--- _deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-BUILD_DIR=${1:-_book}
-cd $BUILD_DIR
-git init
-git add .
-git commit -m "Travis update"
-git push -f "https://${GH_TOKEN}@github.com/${GH_REF}/" master:gh-pages 2>&1 | sed -e "s/${GH_TOKEN}/XXXX/g"
